3a053163e17d186ab170a6681ce72bbdcf5b3707
nicolasholanda <nikolas.nhm14@gmail.com>
2025-11-26 09:43:58 -0300

    Added logic and validation

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,8 +4,24 @@ import InputGroup from 'react-bootstrap/InputGroup';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { useState } from 'react';
 
 function Home() {
+
+    const [username, setUsername] = useState('');
+    const [error, setError] = useState('');
+
+    const searchUsers = (e) => {
+        e.preventDefault();
+        if (!username.trim()) {
+            setError('Username is required');
+            return;
+        }
+        setError('');
+        
+        console.log("Searching users for: ", username);
+    }
+
     return (
         <>
             <Container className="vh-100 d-flex flex-column justify-content-center align-items-center">
@@ -16,16 +32,20 @@ function Home() {
                 </Row>
                 <Row className="w-50">
                     <Col>
-                        <InputGroup className="mb-3">
-                            <Form.Control
-                            placeholder="Enter GitHub username"
-                            aria-label="GitHub username"
-                            aria-describedby="basic-addon2"
-                            />
-                            <Button variant="outline-secondary" id="button-addon2">
-                                Search
-                            </Button>
-                        </InputGroup>
+                        <Form onSubmit={searchUsers}>
+                            <InputGroup className="mb-3" value={username} onChange={(e) => setUsername(e.target.value)}>
+                                <Form.Control
+                                isInvalid={!!error}
+                                placeholder="Enter GitHub username"
+                                aria-label="GitHub username"
+                                aria-describedby="basic-addon2"
+                                />
+                                <Button variant="outline-secondary" id="button-addon2" type="submit">
+                                    Search
+                                </Button>
+                                <Form.Control.Feedback type="invalid">{error}</Form.Control.Feedback>
+                            </InputGroup>
+                        </Form>
                     </Col>
                 </Row>
             </Container>
