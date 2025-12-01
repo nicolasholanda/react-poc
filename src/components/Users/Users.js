fc339fa19f76cac2b49fdd2617cf3ad5b9211f29
nicolasholanda <nikolas.nhm14@gmail.com>
2025-12-01 09:45:23 -0300

    Displaying users

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,14 +1,37 @@
 import { useSearchParams } from "react-router-dom";
+import { searchUsers } from "../../services/githubService";
+import { useEffect, useState } from "react";
+import Card from 'react-bootstrap/Card';
 
 function Users() {
 
     const [searchParams] = useSearchParams();
     const username = searchParams.get('q');
 
+    const [users, setUsers] = useState([]);
+
+    const fetchUsers = async () => {
+        const result = await searchUsers(username);
+
+        console.log(result);
+        setUsers(result);
+    }
+
+    useEffect(() => {
+        fetchUsers();
+    }, []);
+
     return (
-        <div>
-            Users Component {username}
-        </div>
+        <>
+            {users.items?.map(user => (
+                <Card key={user.id} style={{ width: '18rem' }} className="mb-3">
+                    <Card.Img variant="top" src={user.avatar_url} />
+                    <Card.Body>
+                        <Card.Title>{user.login}</Card.Title>
+                    </Card.Body>
+                </Card>
+            ))}
+        </>
     );
 }
 
